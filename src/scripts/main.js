97329179e89cea6710d803d8e5b12430c5cb5795
Artur Misko <arthurmisko@gmail.com>
2024-11-17 00:47:11 +0100

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('.field');
+const lines = table.getElementsByTagName('tr');
+const tableBody = table.querySelector('tbody');
+const counter = lines[0].getElementsByTagName('td');
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+
+appendColumn.addEventListener('click', () => {
+  if (counter.length < 10) {
+    for (const elem of lines) {
+      const newCell = document.createElement('td');
+
+      elem.appendChild(newCell);
+    }
+  }
+});
+
+removeColumn.addEventListener('click', () => {
+  if (counter.length > 2) {
+    for (const elem of lines) {
+      elem.lastElementChild.remove();
+    }
+  }
+});
+
+appendRow.addEventListener('click', () => {
+  if (lines.length < 10) {
+    const newLine = document.createElement('tr');
+
+    tableBody.appendChild(newLine);
+
+    for (let i = 0; i < counter.length; i++) {
+      const newCell = document.createElement('td');
+
+      newLine.appendChild(newCell);
+    }
+  }
+});
+
+removeRow.addEventListener('click', () => {
+  if (lines.length > 2) {
+    tableBody.lastElementChild.remove();
+  }
+});
