f4c0a99093195a7aefdbd530bbeb8ae19045f576
Artur Misko <arthurmisko@gmail.com>
2024-11-17 01:54:31 +0100

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,40 +9,62 @@ const removeColumn = document.querySelector('.remove-column');
 const appendRow = document.querySelector('.append-row');
 const removeRow = document.querySelector('.remove-row');
 
-appendColumn.addEventListener('click', () => {
-  if (counter.length < 10) {
-    for (const elem of lines) {
-      const newCell = document.createElement('td');
-
-      elem.appendChild(newCell);
-    }
+function buttonChecker() {
+  if (counter.length === 10) {
+    appendColumn.disabled = true;
+  } else {
+    appendColumn.disabled = false;
   }
+
+  if (counter.length <= 2) {
+    removeColumn.disabled = true;
+  } else {
+    removeColumn.disabled = false;
+  }
+
+  if (lines.length === 10) {
+    appendRow.disabled = true;
+  } else {
+    appendRow.disabled = false;
+  }
+
+  if (lines.length <= 2) {
+    removeRow.disabled = true;
+  } else {
+    removeRow.disabled = false;
+  }
+}
+
+appendColumn.addEventListener('click', () => {
+  for (const elem of lines) {
+    const newCell = document.createElement('td');
+
+    elem.appendChild(newCell);
+  }
+  buttonChecker();
 });
 
 removeColumn.addEventListener('click', () => {
-  if (counter.length > 2) {
-    for (const elem of lines) {
-      elem.lastElementChild.remove();
-    }
+  for (const elem of lines) {
+    elem.lastElementChild.remove();
   }
+  buttonChecker();
 });
 
 appendRow.addEventListener('click', () => {
-  if (lines.length < 10) {
-    const newLine = document.createElement('tr');
+  const newLine = document.createElement('tr');
 
-    tableBody.appendChild(newLine);
+  tableBody.appendChild(newLine);
 
-    for (let i = 0; i < counter.length; i++) {
-      const newCell = document.createElement('td');
+  for (let i = 0; i < counter.length; i++) {
+    const newCell = document.createElement('td');
 
-      newLine.appendChild(newCell);
-    }
+    newLine.appendChild(newCell);
   }
+  buttonChecker();
 });
 
 removeRow.addEventListener('click', () => {
-  if (lines.length > 2) {
-    tableBody.lastElementChild.remove();
-  }
+  tableBody.lastElementChild.remove();
+  buttonChecker();
 });
